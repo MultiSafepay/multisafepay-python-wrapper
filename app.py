d98ff88ac641fc999330d87aa86294a252e7eadd
Solaiman <solaiman.bacha@multisafepay.com>
2019-06-18 17:59:34 +0200

    Changed sample app code

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from multisafepay.client import Client
 
 msp_client = Client()
+#fill in TEST API key here
 msp_client.set_api_key('')
 
-data = {}
-print(msp_client.order.create(data))
+print(msp_client.order.create({
+    "type": "redirect",
+    "order_id": "my-order-id-1",
+    "gateway": msp_client.paymentmethod.EPS,
+    "currency": "EUR",
+    "amount": "1000",
+    "description": "Test Order Description",
+    "payment_options": {
+        "notification_url": "https://www.example.com/client/notification?type=notification",
+        "redirect_url": "https://www.example.com/client/notification?type=redirect",
+        "cancel_url": "https://www.example.com/client/notification?type=cancel"
+    },
+    "customer": {
+        "locale": "en_US"
+    }
+}))
 
+print(msp_client.gateways.gateways(country='NL',currency='100'))
