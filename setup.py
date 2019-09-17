842ac7b6d3a3ca19f962ac2ea1f0faad8605dbd4
Solaiman <solaiman.bacha@multisafepay.com>
2019-09-17 18:30:47 +0200

    Small changes in setup.py

--- setup.py
+++ setup.py
@@ -10,23 +10,21 @@ def readme():
 setup(
     name='multisafepay-python-wrapper',
     version=version,
-    license='BSD',
+    license='MIT',
     long_description=readme(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
-    description='MultiSafepay Wrapper for Python',
-    author='MultiSafepay B.V.',
-    author_email='integrations@multisafepay.com',
+    description='Python wrapper for the MultiSafepay API',
+    author='MultiSafepay',
+    author_email='integration@multisafepay.com',
     url='https://github.com/MultiSafepay/multisafepay-python-wrapper',
     install_requires=[
         'requests',
     ],
     classifiers=[
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT Licence',
-        'Topic :: Office/Business :: Financial',
     ],
 )
